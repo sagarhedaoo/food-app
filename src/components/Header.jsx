fb8dd985401ed72840fd08aab23858fcc611dec7
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-07-02 01:47:20 -0400

    Half UI done for Create

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,16 +57,28 @@ const Header = () => {
             exit={{ opacity: 0, x: 200 }}
             className="flex items-center gap-8"
           >
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              onClick={() => setisMenu(false)}
+              className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+            >
               Home
             </li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              onClick={() => setisMenu(false)}
+              className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+            >
               Menu
             </li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              onClick={() => setisMenu(false)}
+              className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+            >
               About Us
             </li>
-            <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <li
+              onClick={() => setisMenu(false)}
+              className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+            >
               Service
             </li>
           </motion.ul>
@@ -99,6 +111,7 @@ const Header = () => {
                     <p
                       className="px-2 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
                 transition-all duration-100 ease-in-out text-textColor text-base"
+                      onClick={() => setisMenu(false)}
                     >
                       New Item
                       <MdAdd />
@@ -161,24 +174,28 @@ const Header = () => {
 
               <ul className="flex flex-col">
                 <li
+                  onClick={() => setisMenu(false)}
                   className="text-base hover:bg-slate-100 px-2 py-2  text-textColor hover:text-headingColor  
                 duration-100 transition-all ease-in-out cursor-pointer"
                 >
                   Home
                 </li>
                 <li
+                  onClick={() => setisMenu(false)}
                   className="text-base hover:bg-slate-100 px-2 py-2  text-textColor hover:text-headingColor
                  duration-100 transition-all ease-in-out cursor-pointer"
                 >
                   Menu
                 </li>
                 <li
+                  onClick={() => setisMenu(false)}
                   className="text-base hover:bg-slate-100 px-2 py-2  text-textColor hover:text-headingColor 
                 duration-100 transition-all ease-in-out cursor-pointer"
                 >
                   About Us
                 </li>
                 <li
+                  onClick={() => setisMenu(false)}
                   className="text-base hover:bg-slate-100 px-2 py-2  text-textColor hover:text-headingColor 
                 duration-100 transition-all ease-in-out cursor-pointer"
                 >
